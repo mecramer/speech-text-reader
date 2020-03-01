74b68dc5a8bdaf617b021d63981ac588de0a065f
Cramer <cramerm@nih.gov>
2020-03-01 16:16:18 -0500

    Adding speech functionality

--- script.js
+++ script.js
@@ -77,7 +77,87 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
-  // TODO: speak event
+  // adding a speak event
+  // when we click, it will run functions to set the text and speak the text
+  box.addEventListener('click', () => {
+  setTextMessage(text);
+  speakText();
+
+  // Add active effect by setting a class for .8 seconds
+  box.classList.add('active');
+  setTimeout(() => box.classList.remove('active'), 800);
+});
 
   main.appendChild(box);
 }
+
+// Initialize speech synth
+// SpeechSynthisUtterance is from the API and we're creating a new instance of the object
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+// see https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis for more information
+let voices = [];
+
+// speechSynthesis.getVoices() comes from the API
+// then we loop through each of the voices and add name and language
+// then add to the options html of the page
+function getOurVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach(voice => {
+    const option = document.createElement('option');
+
+    option.value = voice.name;
+    option.innerText = `
+      ${voice.name} ${voice.lang};
+    `;
+
+    voiceSelect.appendChild(option);
+  });
+}
+
+// Set text
+// add the text property of the messages object
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak the text
+// using the API, giving it the message
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+// Set voice
+// change the voice property of the message object to the current value chosen in the dropdown
+// this uses the .find() method
+function setVoice(e) {
+  message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Event Listeners
+
+// Toggle text box on and off the screen
+toggleBtn.addEventListener('click', () => document.querySelector('#text-box').classList.toggle('show'));
+// Close text box with the X button
+closeBtn.addEventListener('click', () => document.querySelector('#text-box').classList.remove('show'));
+
+// Voices Changed
+// run getOurVoices() function whenever select box changes
+speechSynthesis.addEventListener('voiceschanged', getOurVoices);
+
+// Change voice
+// when select dropdown for voices is changes, run the setVoice() function
+voiceSelect.addEventListener('change', setVoice);
+
+// Read text button
+// listen for Read text button
+// when clicked, set the message to the textarea.value, running it in the setTextMessage() function
+// then run speakText() to speak it
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+
+getOurVoices();
